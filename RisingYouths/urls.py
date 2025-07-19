94548c7b171f63e285a7278dde80b73a86b0cac1
ompandey07 <iamompandey.it@gmail.com>
2025-07-19 20:47:01 +0545

    Chair Man Phone Update

--- RisingYouths/urls.py
+++ RisingYouths/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('rising/admin/', admin.site.urls),
     path('', include('Baseapp.routes')),
     path('context/', include('Backend.routes')), 
     path('admin_view/', include('Admin.routes')), 
